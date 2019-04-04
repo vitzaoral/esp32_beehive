75e83056d05face339635c1ce4bcb9bd49485e03
vitzaoral@seznam.cz <vitzaoral@seznam.cz>
2019-04-04 21:26:12 +0200

    Change pin + schema

--- lib/MeteoData/MeteoData.h
+++ lib/MeteoData/MeteoData.h
@@ -6,8 +6,8 @@
 #include <Adafruit_SHT31.h>
 #include <Adafruit_BME280.h>
 
-#define SENSOR_A_PIN 18
-#define SENSOR_B_PIN 19
+#define SENSOR_A_PIN 19
+#define SENSOR_B_PIN 18
 #define SENSOR_C_PIN 5
 
 struct TempAndHumidity
